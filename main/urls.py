25d683f039019097e0adece272697701591128d9
Robee <rob93ghosh@gmail.com>
2021-08-28 10:02:24 +0400

    added logout feature

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from .views import TodoList,TodoListDetail,CreateTask,UpdateTask,DeleteTask,UserLogin
-
+from django.contrib.auth.views import LogoutView
 urlpatterns = [
     path ('',UserLogin.as_view(), name='login'),
+    path ('logout/',LogoutView.as_view(next_page='login'), name='logout'),
     path ('task/',TodoList.as_view(),name='Task'),
     path ('detail/<int:pk>/',TodoListDetail.as_view(),name='detail'),
     path ('createTask/',CreateTask.as_view(),name='createTask'),
